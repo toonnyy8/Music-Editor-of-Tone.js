c5894172d8aa1b7d8b0168626ce04f12eb810f4b
toonnyy8 <toonnyy8@gmail.com>
2019-04-03 14:50:29 +0800

    init

--- src/vue/main.vue
+++ src/vue/main.vue
@@ -12,11 +12,91 @@
 
 <script>
 import * as Tone from "tone";
-var synth = new Tone.PolySynth(6, Tone.Synth).toMaster();
-//set the attributes using the set interface
-synth.set("detune", -1200);
-//play a chord
 
+let frequency = 4;
+var pow = new Tone.Pow(2);
+var synth = new Tone.PolySynth(6, Tone.Synth, {
+  volume: 6,
+  oscillator: {
+    partials: [0, 2, 3, 4]
+  },
+  envelope: {
+    attack: 0.05,
+    decay: 0.01,
+    sustain: 0,
+    release: 4,
+    releaseCurve: "exponential"
+  }
+})
+  .connect(pow)
+  .toMaster();
+
+const poly = new Tone.PolySynth(6, Tone.Synth, {
+  volume: 6,
+  oscillator: {
+    partials: [0, 2, 3, 4]
+  },
+  envelope: {
+    attack: 0.001,
+    decay: 0.17,
+    sustain: 0.0001,
+    release: 0.08,
+    releaseCurve: "exponential"
+  }
+}).toMaster();
+
+import Mousetrap from "mousetrap";
+import "../lib/mousetrap-bind-dictionary.min.js";
+
+Mousetrap.bind({
+  "1": function() {
+    frequency = 1;
+  },
+  "2": function() {
+    frequency = 2;
+  },
+  "3": function() {
+    frequency = 3;
+  },
+  "4": function() {
+    frequency = 4;
+  },
+  "5": function() {
+    frequency = 5;
+  },
+  "6": function() {
+    frequency = 6;
+  },
+  "7": function() {
+    frequency = 7;
+  },
+  "8": function() {
+    frequency = 8;
+  }
+});
+Mousetrap.bind({
+  a: function() {
+    synth.triggerAttackRelease(["C" + frequency], 0.02);
+  },
+  s: function() {
+    synth.triggerAttackRelease(["D" + frequency], 0.02);
+  },
+  d: function() {
+    synth.triggerAttackRelease(["E" + frequency], 0.02);
+  },
+  f: function() {
+    synth.triggerAttackRelease(["F" + frequency], 0.02);
+  },
+  g: function() {
+    synth.triggerAttackRelease(["G" + frequency], 0.02);
+  },
+  h: function() {
+    synth.triggerAttackRelease(["A" + frequency], 0.02);
+  },
+  j: function() {
+    synth.triggerAttackRelease(["B" + frequency], 0.02);
+  }
+});
 export default {
   data: {
     msg: "How Much You Love MET? ",
@@ -27,18 +107,18 @@ export default {
       this.love += 1;
       let frequency = Math.floor(this.love / 10) + 1;
       console.log(frequency);
-      synth.triggerAttackRelease(
+      /*synth.triggerAttackRelease(
         ["C" + frequency, "E" + frequency, "A" + frequency],
         0.02
-      );
+      );*/
     },
     countHate: function() {
       this.love -= 1;
       let frequency = Math.floor(this.love / 10) + 1;
-      synth.triggerAttackRelease(
+      /*synth.triggerAttackRelease(
         ["C" + frequency, "E" + frequency, "A" + frequency],
         0.02
-      );
+      );*/
     }
   }
 };
