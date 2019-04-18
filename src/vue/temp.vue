24f32a71e6f6b63de7c57190db4227375fb6e419
toonnyy8 <toonnyy8@gmail.com>
2019-04-18 19:28:03 +0800

    當主視窗關閉時關閉所有子視窗

--- src/vue/temp.vue
+++ src/vue/temp.vue
@@ -227,10 +227,26 @@ globalChannel.onmessage = function(event) {
   if (event.data.instruction == "Sub Window Creat") {
     let index = event.data.title.split(":")[1].split("-");
 
-    if (vm.data.blocks[index[0]].plugins[index[1]].pluginChannel == null) {
+    if (
+      vm.data.blocks[index[0]].plugins[index[1]] != null &&
+      vm.data.blocks[index[0]].plugins[index[1]].pluginChannel == null
+    ) {
       vm.data.blocks[index[0]].plugins[
         index[1]
       ].pluginChannel = new BroadcastChannel(event.data.title);
+    }
+  }
+};
+window.onunload = function() {
+  for (let i = 0; i < vm.data.blocks.length; i++) {
+    for (let j = 0; j < vm.data.blocks[i].plugins.length; j++) {
+      if (vm.data.blocks[i].plugins[j].pluginChannel != null) {
+        vm.data.blocks[i].plugins[j].pluginChannel.postMessage({
+          instruction: "Close Window"
+        });
+        vm.data.blocks[i].plugins[j].pluginChannel.close();
+        vm.data.blocks[i].plugins[j].pluginChannel = null;
+      }
     }
   }
 };
