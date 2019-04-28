375f0becf7a5ea12a7badae2410f742ef983fdaa
toonnyy8 <toonnyy8@gmail.com>
2019-04-28 22:55:51 +0800

    新增Partials設定

--- src/vue/oscillator.vue
+++ src/vue/oscillator.vue
@@ -14,6 +14,10 @@
                 <h1 align="center">Oscillator</h1>
                 <div align="center" id="Oscillator"></div>
               </div>
+              <div class="demo-card__secondary mdc-typography mdc-typography--body2">
+                <br>
+                <div align="center" id="PartialsCanvas"></div>
+              </div>
             </div>
           </div>
         </div>
@@ -26,15 +30,20 @@
 import { MDCSlider } from "@material/slider";
 
 export default {
+  props: ["setSynth"],
   data() {
     let partials = [32];
     partials.length = 32;
-    for (let i = 0; i < partials.length; i++) {
+    partials[0] = 1;
+    for (let i = 1; i < partials.length; i++) {
       partials[i] = 1;
     }
     return {
       canvas: document.createElement("canvas"),
-      Partials: partials
+      Partials: partials,
+      Frequency: 440,
+      PartialsCanvas: document.createElement("canvas"),
+      partialsMouseMove: false
     };
   },
   mounted() {
@@ -43,11 +52,98 @@ export default {
       this.canvas.height = 240;
       this.canvas.style = "width:100%";
       document.querySelector("#Oscillator").appendChild(this.canvas);
-      this.drawEnvelope();
+      this.drawOscillator();
     }
+    if (this.PartialsCanvas.getContext) {
+      this.PartialsCanvas.width = 1044;
+      this.PartialsCanvas.height = 240;
+      this.PartialsCanvas.style = "width:100%";
+      document
+        .querySelector("#PartialsCanvas")
+        .appendChild(this.PartialsCanvas);
+
+      let ctx = this.PartialsCanvas.getContext("2d");
+      ctx.fillStyle = "rgb(75,75,75)";
+      ctx.fillRect(0, 0, 1044, 240);
+
+      this.PartialsCanvas.onmousedown = event => {
+        this.partialsMouseMove = true;
+        let x = event.offsetX;
+        let y = event.offsetY;
+
+        let offsetWidth = this.PartialsCanvas.offsetWidth;
+        let offsetHeight = this.PartialsCanvas.offsetHeight;
+
+        let cellWidth = 1000 / 1044 / 32;
+
+        x /= offsetWidth;
+        x -= 22 / 1044;
+
+        this.Partials[
+          x < 0 ? 0 : x > 1000 / 1044 ? 31 : Math.floor(x / cellWidth)
+        ] =
+          1 - y / offsetHeight <= 1 / 24
+            ? 0
+            : 1 - y / offsetHeight >= 23 / 24
+            ? 1
+            : 1 - y / offsetHeight;
+
+        this.drawOscillator();
+        this.drawPartials();
+        this.setSynth({
+          oscillator: {
+            frequency: this.Frequency,
+            partials: this.Partials
+          }
+        });
+      };
+      this.PartialsCanvas.onmouseup = event => {
+        this.partialsMouseMove = false;
+      };
+      this.PartialsCanvas.onmousemove = event => {
+        if (this.partialsMouseMove) {
+          let x = event.offsetX;
+          let y = event.offsetY;
+
+          let offsetWidth = this.PartialsCanvas.offsetWidth;
+          let offsetHeight = this.PartialsCanvas.offsetHeight;
+
+          let cellWidth = 1000 / 1044 / 32;
+
+          x /= offsetWidth;
+          x -= 22 / 1044;
+
+          this.Partials[
+            x < 0 ? 0 : x > 1000 / 1044 ? 31 : Math.floor(x / cellWidth)
+          ] =
+            1 - y / offsetHeight <= 1 / 24
+              ? 0
+              : 1 - y / offsetHeight >= 23 / 24
+              ? 1
+              : 1 - y / offsetHeight;
+
+          this.drawOscillator();
+          this.drawPartials();
+          this.setSynth({
+            oscillator: {
+              frequency: this.Frequency,
+              partials: this.Partials
+            }
+          });
+        }
+      };
+      this.drawPartials();
+    }
+
+    this.setSynth({
+      oscillator: {
+        frequency: this.Frequency,
+        partials: this.Partials
+      }
+    });
   },
   methods: {
-    drawEnvelope() {
+    drawOscillator() {
       let ctx = this.canvas.getContext("2d");
       ctx.fillStyle = "rgb(75,75,75)";
       ctx.fillRect(0, 0, 1044, 240);
@@ -77,6 +173,21 @@ export default {
         f += Math.sin(x * Math.PI * 2 * (i + 1)) * this.Partials[i];
       }
       return f;
+    },
+    drawPartials() {
+      let ctx = this.PartialsCanvas.getContext("2d");
+      ctx.fillStyle = "rgb(75,75,75)";
+      ctx.fillRect(0, 0, 1044, 240);
+
+      ctx.fillStyle = "rgb(220,220,220)";
+      for (let i = 0; i < 32; i++) {
+        ctx.fillRect(
+          22 + (i * 1000) / 32,
+          10 + 220 * (1 - this.Partials[i]),
+          Math.ceil(1000 / 32),
+          220 * this.Partials[i]
+        );
+      }
     }
   }
 };
